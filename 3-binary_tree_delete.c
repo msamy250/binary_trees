bb9c69a83a6f96069d2d65df2aae3a8f7e60ea12
Mahmoud Samy <mahmoudsamy@192.168.1.14>
2024-06-07 02:35:20 +0300

    update

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a binary tree node 
+ * @tree: tree to free
+ *
+ * Return: null
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+
+}
